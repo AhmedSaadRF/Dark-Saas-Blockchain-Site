2f361fb1dfb81efe86224b2c89ef14b99066f44f
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-22 13:30:31 +0300

    Upload Project
    Finish Header Section

---
+++ src/sections/Header.tsx
@@ -0,0 +1,30 @@
+export const HeaderSection = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <div>
+            <img src="/assets/images/logo.svg" alt="Blockforge Logo" />
+          </div>
+          <div>
+            <button>
+              <div></div>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 1H12.2667L23 11.7333V24" stroke="currentColor" strokeWidth="2"></path>
+              </svg>
+              <span>Get Started</span>
+            </button>
+            <div>
+              <div>
+                <div></div>
+              </div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
