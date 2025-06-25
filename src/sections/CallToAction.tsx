867a202c19ba27753461905326d18487bd112fb2
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-25 17:47:28 +0300

    Upload Project
    Add Style Classes

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -4,31 +4,31 @@ import { Hexagon } from "../components/Hexagon"
 
 export const CallToAction = () => {
   return (
-    <section>
-      <div>
-        <div>
-          <div>
-            <Hexagon />
+    <section className="py-60 overflow-hidden">
+      <div className="container">
+        <div className="relative">
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Hexagon className="size-[700px]" />
           </div>
-          <div>
-            <Hexagon />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Hexagon className="size-[1100px]" />
           </div>
-          <div>
-            <Circle>
-              <img src="/assets/images/cuboid.png" alt="Cuboid 3D image" />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Circle className="absolute left-0 -top-[400px]">
+              <img src="/assets/images/cuboid.png" alt="Cuboid 3D image" className="size-[140px]" />
             </Circle>
           </div>
-          <div>
-            <Circle>
-              <img src="/assets/images/cylinder.png" alt="Cylinder 3D image" />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Circle className="absolute -left-[600px] -top-[70px]">
+              <img src="/assets/images/cylinder.png" alt="Cylinder 3D image" className="size-[140px]" />
             </Circle>
           </div>
-          <h2>
+          <h2 className="font-heading font-black text-4xl md:text-5xl lg:text-6xl text-center">
             Ready to
-            <span>get started?</span>
+            <span className="block">get started?</span>
           </h2>
-          <p>Start building using blockchain technology, with Blockforge.</p>
-          <div>
+          <p className="text-xl lg:text-2xl text-zinc-400 text-center mt-8 max-w-sm mx-auto">Start building using blockchain technology, with Blockforge.</p>
+          <div className="flex justify-center mt-12">
             <CutCornerButton>Get Started</CutCornerButton>
           </div>
         </div>
@@ -36,4 +36,3 @@ export const CallToAction = () => {
     </section>
   )
 }
-
