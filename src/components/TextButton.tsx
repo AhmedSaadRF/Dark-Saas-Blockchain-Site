8f60bb6e48584ea7901152f06c5f9e4ebb6cc850
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-25 11:35:45 +0300

    Upload Project
    Finish Text Button Component

--- src/components/TextButton.tsx
+++ src/components/TextButton.tsx
@@ -1,1 +1,11 @@
+import type { ComponentPropsWithoutRef } from "react";
+import { twMerge } from "tailwind-merge";
 
+export const TextButton = (props: ComponentPropsWithoutRef<'button'> & {color?: string}) => {
+  const { className, children, color } = props;
+  return (
+    <button>
+      {children}
+    </button>
+  );
+}
