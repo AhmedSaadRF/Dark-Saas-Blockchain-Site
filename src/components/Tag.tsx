5e93ff09282ac1930e78608f4237db6545648980
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-25 14:34:17 +0300

    Upload Project
    Add Style Classes

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -6,7 +6,15 @@ export const Tag = (
 ) => {
   const { children, color, className } = props;
   return (
-    <div>
+    <div
+      className={twMerge(
+        "px-3 py-1.5 uppercase font-heading font-extrabold tracking-wider text-xs bg-fuchsia-500/15 text-fuchsia-500 inline-flex rounded-full",
+        color === "lime" && "bg-lime-500/15 text-lime-500",
+        color === "cyan" && "bg-cyan-500/15 text-cyan-500",
+        color === "violet" && "bg-violet-500/15 text-violet-500",
+        className
+      )}
+    >
       {children}
     </div>
   );
