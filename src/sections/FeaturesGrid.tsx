10a5658df031cfb96f475b3278891a2dd6b9a04c
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-25 11:32:13 +0300

    Upload Project
    Finish Features Grid Section

--- src/sections/FeaturesGrid.tsx
+++ src/sections/FeaturesGrid.tsx
@@ -1,1 +1,65 @@
+import { CutCornerButton } from "../components/CutCornerButton";
+import { TextButton } from "../components/TextButton";
+
+const listItems = [
+  "Experience unparalleled security and scalability",
+  "Fully benefit from scalable network effects",
+  "Unlock the potential of decentralized networks",
+]
+
+export const FeaturesGrid = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          <div>
+            <div>
+              <h2>Empowering the future of blockchain.</h2>
+              <p>Blockforge provides robust and secure infrastructure to support the next generation of decentralized applications.</p>
+              <ul>
+                {listItems.map((item) => (
+                  <li key={item}>
+                    <div>
+                      <div></div>
+                    </div>
+                    <span>{item}</span>
+                  </li>
+                ))}
+              </ul>
+              <div>
+                <CutCornerButton>Get Started</CutCornerButton>
+                <TextButton>Learn More</TextButton>
+              </div>
+            </div>
+            <div>
+              <div>
+                <img src="/assets/images/torus-knot.png" alt="Torus Knot 3D image" />
+                <img src="/assets/images/hemisphere.png" alt="Hemisphere 3D shape" />
+              </div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>
+                <img src="/assets/images/cone.png" alt="Cone 3D shape" />
+                <img src="/assets/images/hemisphere.png" alt="Hemisphere 3D shape" />
+              </div>
+            </div>
+            <div>
+              <h2>Blockforge leads the way.</h2>
+              <div>
+                <p>Blockforge is dedicated to supporting the evolution of Web3 applications by delivering the necessary infrastructure and security for Web3.</p>
+                <p>Blockforge champions Web3 for everyone. As a decentralized blockchain scaling platform, Blockforge enables developers to create scalable, user-friendly dApps with low transaction costs, all while ensuring robust security.</p>
+              </div>
+              <div>
+                <CutCornerButton>Get Started</CutCornerButton>
+                <TextButton>Learn More</TextButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
 
