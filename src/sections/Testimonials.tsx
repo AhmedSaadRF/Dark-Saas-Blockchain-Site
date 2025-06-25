87a35a5072c352c84fa00c938dc987ea65c74c5c
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-25 16:22:15 +0300

    Upload Project
    Finish Testimonials Section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,1 +1,56 @@
 
+import { twMerge } from "tailwind-merge";
+
+const testimonials = [
+  {
+    text: "The user experience is phenomenal, and the support team is always there to help. Highly recommended!",
+    name: "Erica Wyatt",
+    title: "Product Manager - BlockLink",
+    avatarImage: "/assets/images/avatar-erica-wyatt.jpg",
+  },
+  {
+    text: "Our productivity has skyrocketed since we started using Blockforge. it's a game-changer for our team.",
+    name: "Noel Baldwin",
+    title: "Lead Developer - BitBridge",
+    avatarImage: "/assets/images/avatar-noel-baldwin.jpg",
+  },
+  {
+    text: "The integration process was seamless, and the performance improvements are beyond our expectations.",
+    name: "Harry Bender",
+    title: "CTO - CryptoSolutions",
+    avatarImage: "/assets/images/avatar-harry-bender.jpg",
+  },
+];
+
+export const TestimonialsSection = () => {
+  return (
+    <section>
+      <div>
+        <div>
+          {testimonials.map((testimonial, testimonialIndex) => (
+            <blockquote key={testimonialIndex}>
+              <p>
+                &ldquo;{testimonial.text}&ldquo;
+              </p>
+              <cite>
+                <div>
+                  <div>
+                    <div></div>
+                  </div>
+                  <div>
+                    <div>
+                      {testimonial.name}
+                    </div>
+                    <div>
+                      {testimonial.title}
+                    </div>
+                  </div>
+                </div>
+              </cite>
+            </blockquote>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
