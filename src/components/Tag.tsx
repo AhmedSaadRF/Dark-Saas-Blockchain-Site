b5bc8355870affb68a06eef1b84745a84d33a2c4
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-25 14:33:56 +0300

    Upload Project
    Finish Tag Component

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,1 +1,13 @@
+import type { ComponentPropsWithoutRef } from "react";
+import { twMerge } from "tailwind-merge";
 
+export const Tag = (
+  props: ComponentPropsWithoutRef<"div"> & { color?: string }
+) => {
+  const { children, color, className } = props;
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
