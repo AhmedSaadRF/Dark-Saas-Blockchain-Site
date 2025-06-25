1c1a5687851b8b654b5c86b5caa5a8554518a84a
Ahmed Saad <ahmedsaadalrefaey7@gmail.com>
2025-06-25 14:29:37 +0300

    Upload Project

--- src/utils/postUtils.ts
+++ src/utils/postUtils.ts
@@ -1,1 +1,12 @@
-
+export const getPostColorFromCategory = (category: string) => {
+  switch (category) {
+    case "Sustainability":
+      return "lime";
+    case "Innovation":
+      return "cyan";
+    case "Security":
+      return "violet";
+    default:
+      return "fuchsia";
+  }
+};
